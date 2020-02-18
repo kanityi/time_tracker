4a0b1ae6cfe89319914c70b9bc55c5e25b499f6d
Sbu <kanityi@outlook.com>
2020-02-18 23:11:41 +0200

    Finilizing the layout chapter by adding icons on the buttons, also required and assertion for required field also catering for nulls.

--- lib/app/sign_in/sign_in_page.dart
+++ lib/app/sign_in/sign_in_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:time_tracker/app/sign_in/sign_in_button.dart';
+import 'package:time_tracker/app/sign_in/social_sign_in_button.dart';
 
 class SignInPage extends StatelessWidget {
   Widget buildContent() {
@@ -16,34 +18,48 @@ class SignInPage extends StatelessWidget {
               fontWeight: FontWeight.w600,
             ),
           ),
-          SizedBox(
-            height: 8.0,
-          ),
-          RaisedButton(
-            child: Text(
-              'Sign in with google',
-              style: TextStyle(
-                color: Colors.black87,
-                fontSize: 15.0
-              ),
-            ),
-            onPressed: () {
-              print('button pressed');
-            },
+          SizedBox(height: 48.0),
+          SocialSignInButton(
+            assetName: 'images/google-logo.png',
+            text: 'Sign in with Google',
+            textColor: Colors.black87,
             color: Colors.white,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.all(
-                Radius.circular(4.0),
-              ),
+            onPressed: () {},
+          ),
+          SizedBox(height: 8.0),
+          SocialSignInButton(
+            assetName: 'images/facebook-logo.png',
+            text: 'Sign in with Facebook',
+            textColor: Colors.white,
+            color: Color(0xFF334D92),
+            onPressed: () {},
+          ),
+          SizedBox(height: 8.0),
+          SignInButton(
+            text: 'Sign in with Email',
+            textColor: Colors.white,
+            color: Colors.teal[700],
+            onPressed: () {},
+          ),
+          SizedBox(height: 8.0),
+          Text(
+            'Or',
+            style: TextStyle(
+              fontSize: 14.0,
+              color: Colors.black,
             ),
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(height: 8.0),
+          SignInButton(
+            text: 'Go anonynous',
+            textColor: Colors.black,
+            color: Colors.lime[300],
+            onPressed: () {},
           ),
         ],
       ),
     );
-  }
-
-  void _signInWithGoogle() {
-    //TODO: Auth for google
   }
 
   @override
